0c74b70cd7f9e6070460e5db6c9974a28c2e52cd
varun <appuvarungowdab@gmail.com>
2022-08-17 18:48:27 +0530

    End of section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,8 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +17,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 
@@ -31,10 +37,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options => 
-            {
-               options.UseSqlite(_config.GetConnectionString("DefaultConnection")); 
-            });
+            services.AddApplicationServices(this._config);
 
             services.AddControllers();
            services.AddCors((options) =>
@@ -47,6 +50,7 @@ namespace API
                     .WithExposedHeaders("*");
                 });
             });
+           services.AddIdentityServices(this._config);
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -68,6 +72,8 @@ namespace API
             app.UseRouting();
 
            app.UseCors("angularApplication");
+
+           app.UseAuthentication();
             
             app.UseAuthorization();
 
